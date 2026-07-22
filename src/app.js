1db14a9f8dfaf4d29428b8e81bd4e39ede77fa1f
jajai-moto <jajai-moto@gmail.com>
2026-07-22 15:42:51 +0300

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import express from 'express';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-	res.status(200).send('Hello from Acquisitions app.js!');
+  res.status(200).send('Hello from Acquisitions app.js!');
 });
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-	console.log(`\nListening on: https://localhost:${PORT}\n`);
-})
+  console.log(`\nListening on: https://localhost:${PORT}\n`);
+});
